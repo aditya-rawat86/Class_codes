f2283583d73ae31dbe8dec8c7439400de1939051
aditya-rawat86 <rawatsunny747@gmail.com>
2025-09-03 13:14:21 +0530

    codes

---
+++ 12_patterns_question_a.c
@@ -0,0 +1,19 @@
+/*Program to print patterns.
+Part - A*/
+
+#include <stdio.h>
+    int main()
+{
+    int i,j;
+
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
